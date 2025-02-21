963937ed5897e1d82e6f4b3b1124d5502c4add1d
Nadia Attride-Ramella <s2082689@ed.ac.uk>
2025-02-21 13:57:58 +0000

    new data

--- src/ga/optimizer.py
+++ src/ga/optimizer.py
@@ -235,7 +235,7 @@ class GAOptimizer:
         elif isinstance(obj, (np.int_, np.intc, np.intp, np.int8, np.int16, 
                             np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64)):
             return int(obj)
-        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+        elif isinstance(obj, (np.float16, np.float32, np.float64)):
             return float(obj)
         elif isinstance(obj, (list, tuple)):
             return [self._make_json_serializable(item) for item in obj]
